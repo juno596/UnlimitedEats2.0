0fb4da3918a7249c423e9cc5b8fb00393637285c
Juno <junorockman@gmail.com>
2022-12-10 22:35:51 -0500

    added: - form builder - packages for view, controller, and model - added app class - added gears icon

---
+++ src/main/java/Eats/Unlimited/App.java
@@ -0,0 +1,9 @@
+package Eats.Unlimited;
+
+public class App {
+    public static void main(String[] args) {
+
+
+        //when you hover over icons you can see what they are
+    }
+}
---
+++ src/main/java/Eats/Unlimited/View/UEForm.java
@@ -0,0 +1,19 @@
+package Eats.Unlimited.View;
+
+import javax.swing.*;
+
+public class UEForm {
+    private JPanel myUEPanel;
+    private JButton button2;
+    private JButton button3;
+    private JButton getGeneratorButton;
+
+    public UEForm(){
+        Icon generatorIcon = new ImageIcon("gears.png");
+
+        getGeneratorButton = new JButton(generatorIcon);
+    }
+
+
+
+}
